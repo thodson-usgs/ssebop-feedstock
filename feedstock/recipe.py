eb557de6bd59511d56844c124116538ba2ee33f5
Timothy Hodson <34148978+thodson-usgs@users.noreply.github.com>
2024-01-24 09:48:01 -0600

    Update recipe.py

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -24,8 +24,7 @@ def make_url(time: pd.Timestamp) -> str:
     return input_url_pattern.format(yyyyjjj=time.strftime('%Y%j'))
 
 
-# pattern = FilePattern(make_url, ConcatDim(name='time', keys=dates, nitems_per_file=1))
-pattern = FilePattern(make_url, concat_dim='time')
+pattern = FilePattern(make_url, ConcatDim(name='time', keys=dates) #, nitems_per_file=1))
 
 class Preprocess(beam.PTransform):
     """Preprocessor transform."""
